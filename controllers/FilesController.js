a52e01773be7801c1d3d5b40b2d7eb7d8619f7f4
ngugimuchangi <tech.curious14@gmail.com>
2023-04-03 13:58:43 +0300

    file controller method declaration

---
+++ controllers/FilesController.js
@@ -0,0 +1,56 @@
+import dbClient from '../utils/db'
+import redisClient from '../utils/redis'
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function postUpload(req, res) {
+
+}
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function getShow(req, res) {
+
+}
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function getIndex(req, res) {
+
+}
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function putPublish(req, res) {
+
+}
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function putUnpublish(req, res) {
+
+}
+
+/**
+ * 
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ */
+export async function getFile (req, res) {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
-import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
 import postNew from '../controllers/UsersController';
+import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
+import {
+  postUpload, getShow, getIndex, putPublish, putUnpublish, getFile,
+} from '../controllers/FilesController';
 
 // App router
 const router = Router();
@@ -12,5 +15,10 @@ router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
-
+router.post('/files', postUpload);
+router.get('/files:id', getShow);
+router.get('/files', getIndex);
+router.put('/files/:id/publish', putPublish);
+router.put('/files/:id/unpublish', putUnpublish);
+router.put('/files/:id/data', getFile);
 export default router;
