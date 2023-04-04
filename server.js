bb188e38b5de5abc2ea4251020efc608971ae71b
ngugimuchangi <tech.curious14@gmail.com>
2023-04-04 15:23:35 +0300

    used express body parser

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes/index';
 
 // Express server
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
