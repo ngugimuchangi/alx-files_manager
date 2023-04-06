b391d532a80410c603ecfb087da937d33afe0c17
ngugimuchangi <tech.curious14@gmail.com>
2023-04-06 08:09:17 +0300

    authe controller tests

---
+++ tests/AuthController.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import request from 'request';
+import { isUuid } from 'uuid';
+
+/**
+ * Test cases for AuthController.js endpoints:
+ * 1. GET /connect
+ * 2. GET /disconnect
+ * 3. GET /users/me
+ */
+describe('Authentication controller tests', () => {});
