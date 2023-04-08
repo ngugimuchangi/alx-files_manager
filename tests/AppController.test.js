7a0c2fca6d74eccbc055a066de1ff780337d4d8e
ngugimuchangi <tech.curious14@gmail.com>
2023-04-08 19:20:21 +0300

    refactored app controller tests; added auth controller tests

--- tests/AppController.test.js
+++ tests/AppController.test.js
@@ -5,16 +5,14 @@ import sha1 from 'sha1';
 import app from '../server';
 
 chai.use(chaiHttp);
-
+const { expect, request } = chai;
 
 /**
  * Test cases for AppController.js endpoints:
  * 1. GET /status
  * 2. GET /stats
  */
-describe('app controller tests', () => {
-  const { expect } = chai;
-  const { request } = chai;
+describe('App controller tests', () => {
   const DB_HOST = process.env.DB_HOST || 'localhost';
   const DB_PORT = process.env.BD_PORT || 27017;
   const DATABASE = process.env.DB_DATABASE || 'files_manager';
@@ -22,40 +20,50 @@ describe('app controller tests', () => {
   let db;
   const randomString = () => Math.random().toString(16).substring(2);
 
-  before(async () => {
+  before(() => new Promise((resolve) => {
     // Connect to db
     client = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, { useUnifiedTopology: true });
-    await client.connect();
-    db = client.db(DATABASE);
-    // Clear user and files collections
-    await db.collection('users').deleteMany({});
-    await db.collection('files').deleteMany({});
-    // Populate user and files collections
-    for (let i = 0; i < 5; i++) {
-      const newUser = {
-        email: randomString(),
-        password: sha1(randomString()),
-      };
-      const newFile = {
-        name: randomString(),
-        type: 'folder',
-        parentId: 0,
-        isPublic: false,
-      };
-      await db.collection('users').insertOne(newUser);
-      await db.collection('files').insertOne(newFile);
-    }
-  });
+    client.connect(async (error, client) => {
+      if (error) throw error;
+      const users = [];
+      const files = [];
+
+      // Clear user and files collections
+      db = client.db(DATABASE);
+      db.collection('users').deleteMany({});
+      await db.collection('files').deleteMany({});
+
+      // Populate user and files collections
+      for (let i = 0; i < 5; i++) {
+        const newUser = {
+          email: `${randomString()}@mail.com`,
+          password: sha1(randomString()),
+        };
+        const newFile = {
+          name: randomString(),
+          type: 'folder',
+          parentId: 0,
+          isPublic: false,
+        };
+        users.push(newUser);
+        files.push(newFile);
+      }
+      await db.collection('users').insertMany(users);
+      await db.collection('files').insertMany(files);
+      resolve();
+    });
+  }));
 
   after(async () => {
     // Clear collection and close connection
     await db.collection('users').deleteMany({});
     await db.collection('files').deleteMany({});
+    await db.dropDatabase();
     await client.close();
   });
 
   describe('GET /status', () => {
-    it('return status of redis and mongodb clients', (done) => {
+    it('should return status of redis and mongodb clients', (done) => {
       request(app)
         .get('/status')
         .end((error, res) => {
@@ -71,7 +79,7 @@ describe('app controller tests', () => {
     });
   });
   describe('GET /stats', () => {
-    it('returns number of users and files in the database', (done) => {
+    it('should return number of users(5) and files(5) in the database', (done) => {
       request(app)
         .get('/stats')
         .end((error, res) => {
