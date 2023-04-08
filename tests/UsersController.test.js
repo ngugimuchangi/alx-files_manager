b6e502fdf2496ca73283ec06d27bb42a4ef67518
ngugimuchangi <tech.curious14@gmail.com>
2023-04-08 19:49:45 +0300

    user controller unit tests

---
+++ tests/UsersController.test.js
@@ -0,0 +1,54 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { MongoClient } from 'mongodb';
+import app from '../server';
+
+chai.use(chaiHttp);
+const { expect, request } = chai;
+
+/**
+ * Test cases for UsersController.js endpoint:
+ * 1. POST /users
+ */
+describe('Authentication controller tests', () => {
+  let dbClient;
+  let db;
+  const DB_HOST = process.env.DB_HOST || 'localhost';
+  const DB_PORT = process.env.BD_PORT || 27017;
+  const DATABASE = process.env.DB_DATABASE || 'files_manager';
+  const user = { email: 'tester@mail.com', password: 'supersecret' };
+
+  before(() => new Promise((resolve) => {
+    // Connect to db and clear collections
+    dbClient = new MongoClient(`mongodb://${DB_HOST}:${DB_PORT}`, { useUnifiedTopology: true });
+    dbClient.connect(async (error, client) => {
+      if (error) throw error;
+      db = await client.db(DATABASE);
+      await db.collection('users').deleteMany({});
+      resolve();
+    });
+  }));
+
+  after(async () => {
+    // Clear db collections
+    await db.collection('users').deleteOne({ email: user.email });
+    await db.dropDatabase();
+    await dbClient.close();
+  });
+
+  describe('POST /users', () => {
+    it('should create a new a new user and add them to the database', (done) => {
+      request(app)
+        .post('/users')
+        .send(user)
+        .end((error, res) => {
+          expect(error).to.be.null;
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property('id');
+          expect(res.body).to.have.property('email');
+          expect(res.body.email).to.equal(user.email);
+          done();
+        });
+    });
+  });
+});
