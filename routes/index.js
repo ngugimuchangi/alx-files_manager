6fa7c3e2b94898c53aec265dfd73501d2310d676
ngugimuchangi <tech.curious14@gmail.com>
2023-07-23 23:58:03 +0300

    removed docs comments

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,5 @@ router.use(appRouter);
 router.use(authRouter);
 router.use(usersRouter);
 router.use(filesRouter);
-/**
- * @apiDefine XToken
- * @apiHeader {String} X-Token Users access token
- * @apiHeaderExample Header-Example:
- * "X-Token": "a57826f0-c383-4013-b29e-d18c2e68900d"
- */
-/**
- * @apiDefine Unauthorized
- * @apiError UnauthorizedAccess Invalid or missing token
- */
-/**
- * @apiDefine NotFound
- * @apiError NotFound File not found
- */
 
 export default router;
