aed03b77919bc18efde72a4444f06919d0d381fd
ngugimuchangi <tech.curious14@gmail.com>
2023-07-23 23:48:57 +0300

    integrated middleware into server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import router from './routes/index';
+import unmatchedRouteHandler from './middleware/unmatched';
+import errorHandler from './middleware/error';
 
 // Express server
 const app = express();
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+app.use(unmatchedRouteHandler);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
