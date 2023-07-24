2d82b4f1a4f9638d41b567c32cfcdcd8c99c5c55
ngugimuchangi <tech.curious14@gmail.com>
2023-07-25 02:18:09 +0300

    switched to lowercase to ensure method matching

--- middleware/unmatched.js
+++ middleware/unmatched.js
@@ -4,8 +4,8 @@
  * @param {import('express').Response} res - Response object
  */
 function unmatchedRouteHandler(req, res) {
-  if (req.method === 'OPTIONS') res.end();
-  else res.status(404).json({ error: 'Not found' });
+  if (req.method.toLocaleLowerCase() === 'options') res.end();
+  else res.status(404).json({ error: 'Endpoint not found' });
 }
 
 export default unmatchedRouteHandler;
