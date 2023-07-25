b1015aa87adcd384f222e9ff0217f8b9a51b8cdd
ngugimuchangi <tech.curious14@gmail.com>
2023-07-25 03:17:56 +0300

    added gracefully termination handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes/index';
 import unmatchedRouteHandler from './middleware/unmatched';
 import errorHandler from './middleware/error';
+import shutdown from './utils/shutdown';
 
 // Express server
 const app = express();
@@ -13,8 +14,14 @@ app.use(router);
 app.use(unmatchedRouteHandler);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+// Graceful shutdown
+const handler = () => shutdown(server);
+process.on('SIGINT', handler);
+process.on('SIGTERM', handler);
+process.on('SIGQUIT', handler);
 
 export default app;
