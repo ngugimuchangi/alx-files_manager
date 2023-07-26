5a97c9b705ff72b0e0115fe8c00e2f2cbf05f573
ngugimuchangi <tech.curious14@gmail.com>
2023-07-26 09:56:41 +0300

    updated rules -> no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
   rules: {
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
-    'no-console': 'off',
     'no-shadow': 'off',
     'no-restricted-syntax': [
       'error',
